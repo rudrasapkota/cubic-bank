794832e0bb97e99291bec962caf0a27f0207366f
rudrasapkota <prasad.rudrasapkota@gmail.com>
2021-03-16 16:44:25 -0400

    password reset

--- banking-service/src/main/java/com/rab3tech/customer/service/impl/SecurityQuestionService.java
+++ banking-service/src/main/java/com/rab3tech/customer/service/impl/SecurityQuestionService.java
@@ -11,4 +11,7 @@ public interface SecurityQuestionService {
 
 	void save(CustomerSecurityQueAnsVO customerSecurityQueAnsVO);
 
+	List<String> findQuestionAnswer(String email);
+
+	boolean validateQuestionAnswer(String email,String securityAns1,String securityAns2);
 }
